499d934eb0d6299278fa7f01d3cf174ba7674a38
Carlos Segura <carlos.seggch@gmail.com>
2022-07-20 16:26:31 -0500

    Add taxonomies to navbar

--- app/assets/javascript/spree/frontend/all.coffee
+++ app/assets/javascript/spree/frontend/all.coffee
@@ -1,3 +1,5 @@
 #= require jquery
 #= require jquery_ujs
+#= require popper
+#= require bootstrap
 #= require spree
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module SolidusFundamentalsCarlosse
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoloader = :classic
   end
 end
